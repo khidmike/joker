a280ba5d650ef00aa9ae972614f8b561b5f5492a
Michael Khidekel <michael.khidekel@gmail.com>
2015-07-26 16:18:06 -0400

    functionality

--- app/controllers/jokes_controller.rb
+++ app/controllers/jokes_controller.rb
@@ -7,24 +7,4 @@ class JokesController < ApplicationController
     @joke = Joke.where("id = ?", selector)
   end
 
-  def new
-    @joke = Joke.new
-  end
-
-  def create
-      @joke = Joke.new(joke_params)
-       if @joke.save
-         redirect_to joke_path(@joke)
-       else
-         render :index
-       end
-  end
-
-  protected
-
-  def joke_params
-     params.require(:joke).permit(
-       :body
-     )
-  end
 end
